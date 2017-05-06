f4b89ae455c42b9d67e430c7a656ec6486a4fba6
Nitin <office@Nitin-MacBook-Pro.local>
2017-05-07 03:32:56 +0530

    added model.py

--- model.py
+++ model.py
@@ -84,12 +84,12 @@ def main(_):
     ### Model training
     model.fit(X_train, Y_train,validation_split=0.2,shuffle=True)
 
-    # model_json =  model.to_json()
-    # with open("model.json", "w") as json_file:
-    #     json_file.write(model_json)
-    # model.save_weights("model.h5")
-    model.save('model.h5')
-    print("Sa ved model to disk")
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    model.save_weights("model.h5")
+    print("Saved model to disk")
+
 
 
 if __name__ == '__main__':
