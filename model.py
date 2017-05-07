fadf68547f1016fb13d699d6a609cba395e0931d
Nitin <office@Nitin-MacBook-Pro.local>
2017-05-07 23:39:17 +0530

    added cropping layer

--- model.py
+++ model.py
@@ -1,41 +1,27 @@
-import matplotlib.image as mpimg
 import numpy as np
-import pandas
-import random
-from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
 import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Dropout, Lambda, ELU
-from keras.activations import relu, softmax
-from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
-from keras.optimizers import Adam
-from keras.regularizers import l2
-import math
 import csv
 import cv2
 
-colnames = ['center', 'left', 'right', 'steering', 'throttle', 'brake', 'speed']
-data = pandas.read_csv('driving_log.csv', skiprows=[0], names=colnames)
-center_images = data.center.tolist()
-left = data.left.tolist()
-right = data.right.tolist()
-measurements = data.steering.tolist()
-throttle = data.throttle.tolist()
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Dropout, Lambda, ELU
+from keras.layers.convolutional import Convolution2D
+from keras.optimizers import Adam
+from keras.regularizers import l2
+from keras.layers import Cropping2D
 
-# center_images, measurements = shuffle(center_images, measurements)
-# center_images, X_valid, measurements, y_valid = train_test_split(center_images, measurements, test_size=0.10, random_state=100)
-images = []
-meas = []
+
+
 lines = []
+images = []
+measurements = []
+
 with open('driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
 
-images = []
-measurements = []
+
 
 for line in lines:
     source_path = line[0]
@@ -43,14 +29,12 @@ for line in lines:
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
+    #Flip images
     if measurement != 0:
             image_flipped = np.fliplr(image)
             measurement_flipped = - measurement
             images.append(image_flipped)
             measurements.append(measurement_flipped)
-
-
-
 
 X_train = np.array(images)
 Y_train = np.array(measurements)
@@ -58,11 +42,10 @@ Y_train = np.array(measurements)
 
 def main(_):
     print("main function started")
-    # valid_generator = valid_generator(X_valid, y_valid, 128)
-    print("data generated")
     input_shape = (160, 320, 3)
     model = Sequential()
-    model.add(Lambda(lambda x: x / 255 - 0.5, input_shape=input_shape))
+    model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
+    model.add(Lambda(lambda x: x / 255 - 0.5, input_shape=(110,300,3)))
     model.add(Convolution2D(24, 5, 5, border_mode='valid', subsample=(2, 2), W_regularizer=l2(0.001)))
     model.add(Activation('relu'))
     model.add(Convolution2D(36, 5, 5, border_mode='valid', subsample=(2, 2), W_regularizer=l2(0.001)))
